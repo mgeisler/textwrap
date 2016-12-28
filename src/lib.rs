56c4453d4382ffee439afcd6fa614abfb841aa30
Martin Geisler <martin@geisler.net>
2016-12-28 20:25:17 +0100

    Wrapper: introduce struct for holding settings
    As we add more parameters, having a struct to hold everything will be
    convenient.

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,95 @@ extern crate unicode_width;
 use std::iter;
 use unicode_width::UnicodeWidthStr;
 
+/// A Wrapper holds settings for wrapping text.
+pub struct Wrapper {
+    width: usize,
+}
+
+impl Wrapper {
+    /// Create a new Wrapper for wrapping at the specified width.
+    pub fn new(width: usize) -> Wrapper {
+        Wrapper { width: width }
+    }
+
+    /// Fill a line of text at `self.width` bytes.
+    ///
+    /// The result is a string with newlines between each line. Use
+    /// the `wrap` method if you need access to the individual lines.
+    ///
+    /// ```
+    /// use textwrap::Wrapper;
+    ///
+    /// let wrapper = Wrapper::new(15);
+    /// assert_eq!(wrapper.fill("Memory safety without garbage collection."),
+    ///            "Memory safety\nwithout garbage\ncollection.");
+    /// ```
+    pub fn fill(&self, s: &str) -> String {
+        self.wrap(&s).join("\n")
+    }
+
+    /// Wrap a line of text at `self.width` bytes and return a vector
+    /// of lines.
+    ///
+    /// ```
+    /// use textwrap::Wrapper;
+    ///
+    /// let wrap15 = Wrapper::new(15);
+    /// assert_eq!(wrap15.wrap("Concurrency without data races."),
+    ///            vec!["Concurrency",
+    ///                 "without data",
+    ///                 "races."]);
+    ///
+    /// let wrap20 = Wrapper::new(20);
+    /// assert_eq!(wrap20.wrap("Concurrency without data races."),
+    ///            vec!["Concurrency without",
+    ///                 "data races."]);
+    /// ```
+    pub fn wrap(&self, s: &str) -> Vec<String> {
+        let mut result = Vec::new();
+        let mut line = Vec::new();
+        let mut line_width = 0;
+
+        for mut word in s.split_whitespace() {
+            while !word.is_empty() {
+                let splits = split_word(&word);
+                let (smallest, longest) = splits[0];
+                let min_width = smallest.width();
+
+                // Add a new line if even the smallest split doesn't
+                // fit.
+                if !line.is_empty() && line_width + line.len() + min_width > self.width {
+                    result.push(line.join(" "));
+                    line = Vec::new();
+                    line_width = 0;
+                }
+
+                // Find a split that fits on the current line.
+                for &(head, tail) in splits.iter().rev() {
+                    if line_width + line.len() + head.width() <= self.width {
+                        line.push(head);
+                        line_width += head.width();
+                        word = tail;
+                        break;
+                    }
+                }
+
+                // If nothing got added, we forcibly add the smallest
+                // split and continue with the longest tail.
+                if line_width == 0 {
+                    result.push(String::from(smallest));
+                    line_width = 0;
+                    word = longest;
+                }
+            }
+        }
+        if !line.is_empty() {
+            result.push(line.join(" "));
+        }
+        return result;
+    }
+}
+
 /// Fill a line of text at `width` bytes.
 ///
 /// The result is a string with newlines between each line. Use `wrap`
@@ -16,6 +105,10 @@ use unicode_width::UnicodeWidthStr;
 /// assert_eq!(fill("Memory safety without garbage collection.", 15),
 ///            "Memory safety\nwithout garbage\ncollection.");
 /// ```
+///
+/// This function creates a Wrapper on the fly. If you need to wrap
+/// many strings, it can be more efficient to create a single Wrapper
+/// and call its [`fill` method](struct.Wrapper.html#method.fill).
 pub fn fill(s: &str, width: usize) -> String {
     wrap(s, width).join("\n")
 }
@@ -34,47 +127,12 @@ pub fn fill(s: &str, width: usize) -> String {
 ///            vec!["Concurrency without",
 ///                 "data races."]);
 /// ```
+///
+/// This function creates a Wrapper on the fly. If you need to wrap
+/// many strings, it can be more efficient to create a single Wrapper
+/// and call its [`wrap` method](struct.Wrapper.html#method.wrap).
 pub fn wrap(s: &str, width: usize) -> Vec<String> {
-    let mut result = Vec::new();
-    let mut line = Vec::new();
-    let mut line_width = 0;
-
-    for mut word in s.split_whitespace() {
-        while !word.is_empty() {
-            let splits = split_word(&word);
-            let (smallest, longest) = splits[0];
-            let min_width = smallest.width();
-
-            // Add a new line if even the smallest split doesn't fit.
-            if !line.is_empty() && line_width + line.len() + min_width > width {
-                result.push(line.join(" "));
-                line = Vec::new();
-                line_width = 0;
-            }
-
-            // Find a split that fits on the current line.
-            for &(head, tail) in splits.iter().rev() {
-                if line_width + line.len() + head.width() <= width {
-                    line.push(head);
-                    line_width += head.width();
-                    word = tail;
-                    break;
-                }
-            }
-
-            // If nothing got added, we forcibly add the smallest
-            // split and continue with the longest tail.
-            if line_width == 0 {
-                result.push(String::from(smallest));
-                line_width = 0;
-                word = longest;
-            }
-        }
-    }
-    if !line.is_empty() {
-        result.push(line.join(" "));
-    }
-    return result;
+    Wrapper::new(width).wrap(s)
 }
 
 /// Split word into all possible parts (head, tail). Word must be
