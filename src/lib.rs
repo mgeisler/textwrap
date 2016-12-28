9634c919ceacb4565ae513cf4734222a0bb369f8
Martin Geisler <martin@geisler.net>
2016-12-28 17:31:38 +0100

    wrap: reformulate in terms of possible splits
    The main algorithm now works in terms of a number of possible splits of
    the current word. This should make it easy to extend the algorithm to
    take things like hyphens into account in the future.

--- src/lib.rs
+++ src/lib.rs
@@ -38,20 +38,48 @@ pub fn wrap(s: &str, width: usize) -> Vec<String> {
     let mut line = Vec::new();
     let mut line_width = 0;
 
-    for word in s.split_whitespace() {
-        let word_width = word.width();
-        if !line.is_empty() && line_width + line.len() + word_width > width {
-            result.push(line.join(" "));
-            line = Vec::new();
-            line_width = 0;
+    for mut word in s.split_whitespace() {
+        while !word.is_empty() {
+            let splits = split_word(&word);
+            let (smallest, longest) = splits[0];
+            let min_width = smallest.width();
+
+            // Add a new line if even the smallest split doesn't fit.
+            if !line.is_empty() && line_width + line.len() + min_width > width {
+                result.push(line.join(" "));
+                line = Vec::new();
+                line_width = 0;
+            }
+
+            // Find a split that fits on the current line.
+            for &(head, tail) in splits.iter().rev() {
+                if line_width + line.len() + head.width() <= width {
+                    line.push(head);
+                    line_width += head.width();
+                    word = tail;
+                    break;
+                }
+            }
+
+            // If nothing got added, we forcibly add the smallest
+            // split and continue with the longest tail.
+            if line_width == 0 {
+                result.push(String::from(smallest));
+                line_width = 0;
+                word = longest;
+            }
         }
-        line.push(word);
-        line_width += word_width;
     }
     if !line.is_empty() {
         result.push(line.join(" "));
     }
     return result;
+}
+
+/// Split word into all possible parts (head, tail). Must return a
+/// non-empty vector.
+fn split_word(word: &str) -> Vec<(&str, &str)> {
+    return vec![(word, "")];
 }
 
 #[cfg(test)]
