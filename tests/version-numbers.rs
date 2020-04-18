851905b036368d1c21d8e7ef84c69c587d3b6904
Martin Geisler <martin@geisler.net>
2020-04-18 15:57:54 +0200

    travis: stop testing with Rust 1.31
    Over the years, we’ve repeatedly seen build failures in our CI, even
    when nothing changed in `textwrap`. The failures happened because we
    tested against a fixed version of Rust, but our dependencies kept
    releasing new patch it has versions that would push up the minimum
    required Rust version.
    
    The build failures makes it infeasible to keep `textwrap` compatible
    with any particular version of Rust. We will therefore track the
    latest stable version of Rust from now on.

--- tests/version-numbers.rs
+++ tests/version-numbers.rs
@@ -1,7 +1,3 @@
-use std::fs::File;
-use std::io::Read;
-use yaml_rust::YamlLoader;
-
 #[test]
 fn test_readme_deps() {
     version_sync::assert_markdown_deps_updated!("README.md");
@@ -13,33 +9,6 @@ fn test_readme_changelog() {
         "README.md",
         r"^### Version {version} — .* \d\d?.., 20\d\d$"
     );
-}
-
-#[test]
-fn test_readme_rustc_min_version() {
-    let mut file = File::open(".travis.yml").expect("Could not open");
-    let mut buf = String::new();
-    file.read_to_string(&mut buf).expect("Could not read");
-
-    let yaml = YamlLoader::load_from_str(&buf).expect("Could not parse");
-    assert_eq!(yaml.len(), 1, "Expected a single YAML document");
-
-    let rust_versions = &yaml[0]["rust"]
-        .as_vec()
-        .expect("Could not find vector with Rust versions");
-
-    let min_rust_version = rust_versions
-        .iter()
-        .filter_map(|version| match version.as_str() {
-            Some("stable") | Some("beta") | Some("nightly") => None,
-            version => version,
-        })
-        .last()
-        .expect("No minimum Rust version found");
-
-    let pattern = "https://img.shields.io/badge/rustc-{version}-4d76ae.svg";
-    version_sync::check_contains_regex("README.md", pattern, "", min_rust_version)
-        .expect("Minimum Rust version not found");
 }
 
 #[test]
