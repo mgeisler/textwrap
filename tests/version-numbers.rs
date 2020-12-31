ff0ae9d88927dbfdde7f215c7c374062baa9b407
Martin Geisler <martin@geisler.net>
2020-12-31 11:11:57 +0100

    Include a dependency graph in the generated documentation
    This used cargo-depgraph to generate a dependency graph which we
    convert to a SVG image. The image is committed to the repository under
    a versioned filename. This allows us to link to the correct image in
    the documentation.
    
    The dependency graph is updated when new tags are pushed — this should
    ensure that the images are kept up to date.

--- tests/version-numbers.rs
+++ tests/version-numbers.rs
@@ -15,3 +15,8 @@ fn test_changelog() {
 fn test_html_root_url() {
     version_sync::assert_html_root_url_updated!("src/lib.rs");
 }
+
+#[test]
+fn test_dependency_graph() {
+    version_sync::assert_contains_regex!("src/lib.rs", "master/images/textwrap-{version}.svg");
+}
