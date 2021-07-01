ac9c654e435706f50509c450aae0a0388d112df8
Robin Krahl <robin.krahl@ireas.org>
2021-07-01 12:16:38 +0200

    Configure required features for examples
    Instead of manually checking for required features in the hyphenation
    and termwidth examples, we can configure the required features in
    Cargo.toml.  This makes the examples easier to read and maintain.

--- examples/hyphenation.rs
+++ examples/hyphenation.rs
@@ -1,14 +1,5 @@
-#[cfg(feature = "hyphenation")]
 use hyphenation::{Language, Load, Standard};
 
-#[cfg(not(feature = "hyphenation"))]
-fn main() {
-    println!("Please run this example as");
-    println!();
-    println!("  cargo run --example hyphenation --features hyphenation");
-}
-
-#[cfg(feature = "hyphenation")]
 fn main() {
     let text = "textwrap: a small library for wrapping text.";
     let dictionary = Standard::from_embedded(Language::EnglishUS).unwrap();
