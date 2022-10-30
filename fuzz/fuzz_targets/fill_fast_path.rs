13263c9f81e72f04c99d0c54c0c190fe79077f70
Martin Geisler <martin@geisler.net>
2022-10-30 10:21:11 +0100

    Limit inputs to prevent OSS-Fuzz timeouts
    I downloaded the failing test cases from OSS-Fuzz and ran the fuzzers
    on each of them: they all finished in ~35 seconds on my laptop. The
    timeout on OSS-Fuzz is 25 seconds, which explains the timeouts.
    
    Limiting the input length to 100k bytes should help here and still
    allow us to test all reasonable inputs.

--- fuzz/fuzz_targets/fill_fast_path.rs
+++ fuzz/fuzz_targets/fill_fast_path.rs
@@ -2,6 +2,10 @@
 use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|input: (String, usize)| {
+    if input.0.len() > 100_000 {
+        return; // Avoid timeouts in OSS-Fuzz.
+    }
+
     let options = textwrap::Options::new(input.1);
     let fast = textwrap::fill(&input.0, &options);
     let slow = textwrap::fuzzing::fill_slow_path(&input.0, options);
