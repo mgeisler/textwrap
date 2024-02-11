3dd0774cc3ead0a441ab08d26aa4fb28d99cac86
Martin Geisler <martin@geisler.net>
2024-02-11 07:58:38 +0000

    Add dependency graph for version 0.16.1

--- src/lib.rs
+++ src/lib.rs
@@ -113,7 +113,7 @@
 //! The full dependency graph, where dashed lines indicate optional
 //! dependencies, is shown below:
 //!
-//! <img src="https://raw.githubusercontent.com/mgeisler/textwrap/master/images/textwrap-0.16.0.svg">
+//! <img src="https://raw.githubusercontent.com/mgeisler/textwrap/master/images/textwrap-0.16.1.svg">
 //!
 //! ## Default Features
 //!
