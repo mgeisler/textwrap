12616b8e43a8bc9e84efa4f852db3537a7a93a9a
Augie Fackler <raf@durin42.com>
2026-04-20 02:02:42 -0400

    word_separators: switch from unicode-linebreak to icu_segmenter (#608)
    * word_separators: switch from unicode-linebreak to icu_segmenter
    
    The former hasn't been touched in a while and has a dep on an old
    hashbrown, which is what caused me to notice the dep in the first place.
    icu_segmenter is maintained by the Unicode Consortium which seems like
    a more canonical solution anyway.
    
    * Run `dprint fmt`
    
    * Bump MSRV to 1.18 for `zerofrom v0.1.6`
    
    * Update example and benchmark lock files
    
    They needed a bump after the introduction of `icu_segmenter`.
    
    ---------
    
    Co-authored-by: Martin Geisler <martin@geisler.net>

--- src/lib.rs
+++ src/lib.rs
@@ -120,7 +120,7 @@
 //! These features are enabled by default:
 //!
 //! * `unicode-linebreak`: enables finding words using the
-//!   [unicode-linebreak] crate, which implements the line breaking
+//!   [icu_segmenter] crate, which implements the line breaking
 //!   algorithm described in [Unicode Standard Annex
 //!   #14](https://www.unicode.org/reports/tr14/).
 //!
@@ -185,7 +185,7 @@
 //!   [hyphenation] crate. See the [`word_splitters::WordSplitter`]
 //!   trait for details.
 //!
-//! [unicode-linebreak]: https://docs.rs/unicode-linebreak/
+//! [icu_segmenter]: https://docs.rs/icu_segmenter/
 //! [unicode-width]: https://docs.rs/unicode-width/
 //! [smawk]: https://docs.rs/smawk/
 //! [binary-sizes demo]: https://github.com/mgeisler/textwrap/tree/master/examples/binary-sizes
